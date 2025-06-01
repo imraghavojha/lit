db4dedf18e3757e0fe15a677880b63852052de6b
RaghavOjha <jeh267@txstate.edu>
2025-05-31 23:55:40 -0500

    working blobObject class
    was able to get sha1 id based on content

---
+++ Main.java
@@ -0,0 +1,11 @@
+import objects.BlobObject;
+
+public class Main{
+    public static void main(String[] args) {
+        BlobObject blob1 = new BlobObject("sample/blobSample.txt");
+
+        String sample_sha1 = blob1.getSha1();
+
+        System.out.println("SHA-1: "+ sample_sha1);
+    }
+}
---
+++ src/objects/BlobObject.java
@@ -0,0 +1,52 @@
+package objects;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import utils.Content;
+
+public class BlobObject {
+    private String blobSha1;
+    private byte[] content;
+    private final String path;
+
+    public BlobObject(String filePath) {
+        this.path = filePath;
+        try {
+            this.content = Content.fileToByte(path);
+        } catch (IOException e) {
+            System.err.println("Error getting content.");
+        } catch (Exception e){
+            System.err.println("An unexpected error occurred while loading blob content: " + e.getMessage());
+        }
+    }
+
+    private String calculateSha1() {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] hash = md.digest(content);
+            blobSha1 = bytesToHex(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        return blobSha1;
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    public String getSha1(){
+        return calculateSha1();
+    } 
+}
