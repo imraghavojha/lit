111967b2e80596518ef8aaaa3a2da5400245956e
RaghavOjha <jeh267@txstate.edu>
2025-06-08 17:35:06 -0500

    Tested the commitObject in main

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import java.nio.file.Paths;
 import objects.BlobObject;
 import objects.TreeObject;
 import utils.TreeBuilder;
+import objects.CommitObject;
 
 public class Main{
     public static void main(String[] args) {
@@ -41,6 +42,32 @@ public class Main{
                                    ", SHA-1: " + entry.getObjectSha1Id() +
                                    ", Name: " + entry.getName());
             }
+
+            System.out.println("\n----------------------------------------\n");
+
+            // --- Commit Testing ---
+            System.out.println("--- Creating First Commit ---");
+            
+            // 1. Get the root tree SHA-1 from the previous step
+            String rootTreeSha1 = rootTree.getSha1Id();
+            
+            // 2. For the first commit, there is no parent
+            String parentSha1 = null; 
+            
+            // 3. Define author details and commit message
+            String authorName = "Raghav Ojha";
+            String authorEmail = "imraghavojha@gmail.com";
+            String commitMessage = "Initial commit: Add sample files";
+            
+            // 4. Create the commit object
+            CommitObject initialCommit = new CommitObject(rootTreeSha1, parentSha1, authorName, authorEmail, commitMessage);
+            
+            System.out.println("New Commit Object Created!");
+            System.out.println("Commit SHA-1: " + initialCommit.getSha1());
+            System.out.println("Tree SHA-1:   " + initialCommit.getTreeSha1());
+            System.out.println("Parent SHA-1: " + initialCommit.getParentSha1());
+            System.out.println("Message:      " + initialCommit.getCommitMessage());
+
 
         } catch (IOException e) {
             System.err.println("An I/O error occurred during tree building: " + e.getMessage());
