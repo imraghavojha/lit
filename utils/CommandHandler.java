5c088a6d935c041fa7867f8c7f85d8b199bee3fe
Kusum Bhattarai Sharma <xcm15@txstate.edu>
2025-06-19 21:02:20 -0500

    Created other subdirectories
    HEAD for branch pointer and refs to store commit SHA-1s for branches, objects for database

--- utils/CommandHandler.java
+++ utils/CommandHandler.java
@@ -9,10 +9,22 @@ public class CommandHandler {
     public static void handleInit(String command){
         if (command.equals("init")){
             Path litPath = Paths.get ("sample/.lit");
+            Path objectsPath = Paths.get("sample/.lit/objects");
+            Path refPath = Paths.get("sample/.lit/refs/heads");
+            Path HEADPath = Paths.get("sample/.lit/HEAD");
+            Path mainPath = Paths.get("sample/.lit/refs/heads/main");
             try {
-                
+                // creating .lit directory and subdirectories
                 Files.createDirectory(litPath);
-                System.out.println("Yay! Succesfully created .lit/!");
+                
+                Files.createDirectory(objectsPath);
+
+                Files.createDirectories(refPath);
+
+                Files.createFile(HEADPath);
+
+                Files.createFile(mainPath);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
