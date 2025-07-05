9ab0be6503c18f5551f85191a810e18cd254bd4b
Kusum Bhattarai Sharma <xcm15@txstate.edu>
2025-07-05 00:31:23 -0500

    Integrated the switch case for branch command

--- Main.java
+++ Main.java
@@ -28,6 +28,21 @@ public class Main{
                 }
                 break;
 
+            case "branch": 
+                if (args.length < 2) {
+                    System.err.println("Error: No branch name specified for 'branch' command.");
+                    System.out.println("Usage: lit branch <branch_name>");
+                    return;
+                }
+                String branchName = args[1]; // Get the branch name from the arguments
+                try {
+                    CommandHandler.handleBranch(branchName); // Call the new handleBranch method
+                } catch (Exception e) {
+                    System.err.println("Error creating branch '" + branchName + "': " + e.getMessage());
+                }
+                break;
+
+
         // Future commands to be added here
             default:
                 System.err.println("Non-existing command!");
