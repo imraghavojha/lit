9f62cd988d50ed57b2318fac2233a76a92b38265
Kusum Bhattarai Sharma <xcm15@txstate.edu>
2025-07-07 14:07:46 -0500

    helper method to convert byte array to hex string

--- utils/ObjectLoader.java
+++ utils/ObjectLoader.java
@@ -166,4 +166,15 @@ public class ObjectLoader {
 
         return tree;
     }
+    //  helper for converting bytes to hex 
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
 }
