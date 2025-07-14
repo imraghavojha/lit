edbfaaadf2f24e694a0aeae4f34a89d5a11a849e
RaghavOjha <jeh267@txstate.edu>
2025-07-14 15:58:35 -0500

    added FileDiffer which compares diff btw two files

---
+++ src/main/java/utils/ChangeType.java
@@ -0,0 +1,9 @@
+package com.lit.utils; 
+
+/**
+ * enum to represent the type of change for a line in a diff.
+ */
+public enum ChangeType {
+    ADDED,
+    DELETED
+}
---
+++ src/main/java/utils/DiffResult.java
@@ -0,0 +1,35 @@
+package com.lit.utils; 
+
+import java.util.List;
+
+/**
+ * class to hold the structured result of a file comparison.
+ */
+public class DiffResult {
+    private final List<DiffLine> diffLines;
+
+    public DiffResult(List<DiffLine> diffLines) {
+        this.diffLines = diffLines;
+    }
+
+    public List<DiffLine> getDiffLines() {
+        return diffLines;
+    }
+
+    public boolean hasChanges() {
+        return !diffLines.isEmpty();
+    }
+}
+
+/**
+ * single line in a diff output, with its type (ADDED/DELETED) and text.
+ */
+class DiffLine {
+    public final ChangeType type;
+    public final String text;
+
+    public DiffLine(ChangeType type, String text) {
+        this.type = type;
+        this.text = text;
+    }
+}
