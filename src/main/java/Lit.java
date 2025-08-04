dd9cf15ad4500b97b4583938c45b0a464b6adbf1
Kusum Bhattarai Sharma <xcm15@txstate.edu>
2025-08-03 22:34:17 -0500

    Added picocli runnable
    Lit class with runnable interface implementation and will be responsible for running subcommands, Main will just execute Lit runnable

---
+++ src/main/java/Lit.java
@@ -0,0 +1,22 @@
+import commands.InitCommand;
+import picocli.CommandLine;
+import picocli.CommandLine.Command; 
+
+@Command(
+    name = "lit",
+    mixinStandardHelpOptions = true,
+    version = "Lit 1.0",
+    description = "A custom version control system built in Java.",
+    // commands
+    subcommands = {
+        InitCommand.class
+        // more to be added here
+    }
+)
+public class Lit implements Runnable {
+    // ... rest of the class is unchanged ...
+    @Override
+    public void run() {
+        new CommandLine(this).usage(System.out);
+    }
+}
